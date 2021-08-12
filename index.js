744f63eab52be1c321d88eaf30bbd4bf349af791
Annabelle Yee <78177094+Annabelleyeee@users.noreply.github.com>
2021-08-12 22:17:06 +0000

    Game is now somewhat functional, added in boundaries to answers and ability to award points

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log('Welcome to this Card Guessing Game')
+console.log('Welcome to this Card Guessing Game!\n\nA card will be drawn from a deck and it is your job to guess if the next card drawn will have a higher or lower card number value on it.\nIf you guess correctly then you will recieve a point!\nAt the end of the game, the person with the most points wins.')
+
 //Asking for the user's names
 let definedName1 = prompt ('You are player 1, What is your name?')
 let definedName2 = prompt ('You are player 2, What is your name?')
@@ -36,26 +37,63 @@ let pointPlayer2 = 0;
 play();
 function play() {
 
-//the varible random will produce a random card number between 1 and 13
+//the varible randomCard will produce a random card number between 1 and 13. "nextRandomCard" will assign another number between 1 - 13 for the next card drawn.
 randomCard = Math.floor(Math.random() * 13 + 1);
+nextRandomCard = Math.floor(Math.random() * 13 + 1);
 
 
 //main game = each player is presented with a random card number and has to guess if the following card number drawn will be higher or lower.
 do{
-  console.log('The card drawn has the number' + randomCard)
+  console.log('The card drawn has the number ' + randomCard)
+
+
   var guessPlayer1 = prompt( player1 + ', do you think the next card will have a higher or lower number?');
 
-    if (guessPlayer1 == higher && randomCard){
-			console.log('Incorrect');
-		} else if (guessPlayer1 < random) {
-			console.log('Your guess is too low');
-		} else {
-      // if they get is correct then a point is added
-			console.log('You got it correct');
-    pointPlayer1++
-		console.log('You have ' + pointPlayer1 + 'points');
+      if (guessPlayer1=='' || guessPlayer1==null) {
+          console.log('Error: no input');
+          guessPlayer1 = prompt( player1 + ', do you think the next card will have a higher or lower number?');
+  
+      } else if (guessPlayer1 != 'higher' && guessPlayer1 != 'lower') {
+        console.log('Error: Invalid response, please type \'higher\' or \'lower\'.');
+        guessPlayer1 = prompt( player1 + ', do you think the next card will have a higher or lower number?');
+
+}
+
+
+  var guessPlayer2 = prompt( player2 + ', do you think the next card will have a higher or lower number?');
+
+      if (guessPlayer1=='' || guessPlayer1==null) {
+          console.log('Error: no input')
+          guessPlayer1 = prompt( player1 + ', do you think the next card will have a higher or lower number?');
+  
+      } else if (guessPlayer1 != 'higher' && guessPlayer1 != 'lower') {
+        console.log('Error: Invalid response, please type \'higher\' or \'lower\'.')
+      guessPlayer2 = prompt( player2 + ', do you think the next card will have a higher or lower number?');
+}
+
+    if (guessPlayer1=='higher'&&randomCard<nextRandomCard 
+    ||  guessPlayer1=='lower'&&randomCard>nextRandomCard) {
+			console.log(player1 + ' you are correct!');
+      pointPlayer1++
+
+		} else if (guessPlayer1=='higher'&&randomCard>nextRandomCard 
+    ||  guessPlayer1=='lower'&&randomCard<nextRandomCard) {
+			console.log(player1 + ' you are incorrect!');
+		} 
+
+    if (guessPlayer2=='higher'&&randomCard<nextRandomCard 
+    ||  guessPlayer2=='lower'&&randomCard>nextRandomCard) {
+			console.log(player2 + ' you are correct!');
+      pointPlayer1++
+
+		} else if (guessPlayer2=='higher'&&randomCard>nextRandomCard 
+    ||  guessPlayer2=='lower'&&randomCard<nextRandomCard) {
+			console.log(player2 + ' you are incorrect!');
+		} 
+
+    console.log('The next card drawn had a number value of ' + nextRandomCard)
     break;
-    }
+    
 round++
 
   
@@ -72,7 +110,6 @@ let again = prompt ('Do you want to play again?');
 if (again =='yes') {
   play();
 } else{
-  console.log('Your final score is ' + point)
 	console.log('Game Over');
 }
 }
